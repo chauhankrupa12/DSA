3046a1f37780f6afc4939801bc66753d6876eec8
krupa <chauhankrupa232@gmail.com>
2026-09-21 07:59:02 +0530

    variables1

---
+++ variables.py
@@ -0,0 +1,44 @@
+"""variables"""
+ 
+# store name,age and city
+name = "Atmiya"
+age = 20
+city = "Ahmedabad"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+#swap two values
+a = 10
+b = 20
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
+
+#area of rectangle
+length = 10
+width = 5
+
+area = length * width
+
+print("Area of rectangle:", area)
+
+#simple interest
+p = 10000
+r = 5
+t = 2
+
+si = (p * r * t) / 100
+
+print("Simple Interest:", si)
+
+#celsius to fehrenhit
+celsius = 25
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Fahrenheit:", fahrenheit)
+
